131d6a21bff37baf7cafaf3f462da519e2121d6a
Oliver Knight <oliver.knight@charite.de>
2024-02-11 21:36:24 +0100

    Added build

--- singularity/build.sh
+++ singularity/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 
-echo "First, you need to download cellranger-7.2.0.tar.gz AND cellranger-atac-2.1.0.tar.gz into this folder."
+echo "First, you need to download cellranger-7.2.0.tar.gz AND cellranger-atac-2.1.0.tar.gz ABOVE this folder, then come back."
 echo "You can download them from here:"
 echo "https://www.10xgenomics.com/support/software/cell-ranger/downloads"
 echo "https://support.10xgenomics.com/single-cell-atac/software/downloads/latest"
@@ -10,12 +10,15 @@ read -r choice
 
 # Process choices
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
-	echo "apptainer build oscar-counting.sif recipe_oscar-counting.sif"
-	apptainer build oscar-counting.sif recipe_oscar-counting.sif
+
+	cd ..
+
+	echo "apptainer build oscar-counting.sif singularity/recipe_oscar-counting.sif"
+	apptainer build oscar-counting.sif singularity/recipe_oscar-counting.sif
 	echo ""
 
-        echo "apptainer build oscar-qc.sif recipe_oscar-qc.sif"
-        apptainer build oscar-qc.sif recipe_oscar-qc.sif
+        echo "apptainer build oscar-qc.sif singularity/recipe_oscar-qc.sif"
+        apptainer build oscar-qc.sif singularity/recipe_oscar-qc.sif
 elif [ "$choice" = "N" ] || [ "$choice" = "n" ]; then
     :
 else
