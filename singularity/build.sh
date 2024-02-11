ae1e87934af3718dd73a0791e7fb5125862216e8
ollieeknight <oliver.knight@drfz.de>
2024-02-11 12:10:05 +0100

    Added singularity build files

---
+++ singularity/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+
+echo "First, you need to download cellranger-7.2.0.tar.gz AND cellranger-atac-2.1.0.tar.gz into this folder."
+echo "You can download them from here:"
+echo "https://www.10xgenomics.com/support/software/cell-ranger/downloads"
+echo "https://support.10xgenomics.com/single-cell-atac/software/downloads/latest"
+
+echo "Are you sure you want to build these singularity files? (Y/N)"
+read -r choice
+
+# Process choices
+if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
+	echo "apptainer build oscar-counting.sif recipe_oscar-counting.sif"
+	apptainer build oscar-counting.sif recipe_oscar-counting.sif
+	echo ""
+
+        echo "apptainer build oscar-qc.sif recipe_oscar-qc.sif"
+        apptainer build oscar-qc.sif recipe_oscar-qc.sif
+elif [ "$choice" = "N" ] || [ "$choice" = "n" ]; then
+    :
+else
+    echo "Invalid choice. No worries"
+fi
