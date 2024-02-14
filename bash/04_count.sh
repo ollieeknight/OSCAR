06cccf167b9ab057f5fee05bc4397e52d476e6f2
Oliver Knight <oliver.knight@charite.de>
2024-02-15 00:00:17 +0100

    Added further checks and info messages

--- bash/04_count.sh
+++ bash/04_count.sh
@@ -33,6 +33,7 @@
 # Define default values
 OSCAR_script_dir=$(dirname "${BASH_SOURCE[0]}")
 prefix="$HOME/scratch/ngs"
+num_cores=$(nproc)
 
 # Parse command-line arguments
 while [[ $# -gt 0 ]]; do
@@ -60,27 +61,48 @@ if [ -z "$output_project_id" ]; then
     exit 1
 fi
 
-container=$TMPDIR/oscar-counting_v1.sif
+read_length=$(awk -F '"' '/<Read Number="1"/ {print $4}' ${prefix}/${output_project_id}/${output_project_id}_bcl/RunInfo.xml)
+if [ "$read_length" -gt 45 ]; then
+    run_type="ATAC"
+elif [ "$read_length" -lt 45 ]; then
+    run_type="GEX"
+else
+    echo -e "\033[0;31mERROR:\033[0m Cannot determine run type, please check ${project_dir}/${output_project_id}_bcl/RunInfo.xml"
+    exit 1
+fi
+
+echo ""
+echo -e "\033[34mINFO:\033[0m $output_project_id is an $run_type run, processing appropriately"
+
+project_dir="${prefix}/$output_project_id"
+library_folder="${project_dir}/${output_project_id}_scripts/libraries"
+fastq_dir=${project_dir}/${output_project_id}_fastq
+mkdir -p $fastq_dir/logs
+
+# Check if indices folder does not exist, and exit if it's not present
+if [ ! -d "$library_folder" ]; then
+    echo -e "\033[0;31mERROR:\033[0m Libraries folder does not exist, please run the process_metadata script"
+    exit 1
+fi
+
+# Check the symbolic link for the group folder in the users $HOME
+if [ ! -d "$HOME/group" ]; then
+    ln -s /fast/work/groups/ag_romagnani/ $HOME/group
+fi
+
+container=$TMPDIR/oscar-counting_latest.sif
 
 # Check that the singularity container is available
 if [ ! -f "${container}" ]; then
-    echo "oscar-counting_v1.sif singularity file not found, pulling..."
-    apptainer pull --arch amd64 library://romagnanilab/default/oscar-counting:v1
-fi
-
-# Define the project directory
-project_dir=$project_dir/$output_project_id
-library_folder=${prefix}/${output_project_id}/${output_project_id}_scripts/libraries
-outs="${prefix}/${output_project_id}/${output_project_id}_outs"
-mkdir -p "$outs"
-
-if [ ! -f "$library_folder" ]; then
-    echo -e "\033[0;31mERROR:\033[0m Libraries folder not found - did you run the last script (process_libraries.sh)?"
-    exit 1
+    echo "oscar-counting_latest.sif singularity file not found, pulling..."
+    mkdir -p $TMPDIR
+    apptainer pull --dir $TMPDIR library://romagnanilab/default/oscar-counting:latest
 fi
 
 # Take the csv files into a list and remove the .csv suffix
 libraries=($(ls "$library_folder" | awk -F/ '{print $NF}' | awk -F. '{print $1}'))
+outs=${project_dir}/${output_project_id}_outs/
+mkdir -p ${outs}/
 
 # Iterate over each library file to submit counting jobs
 for library in "${libraries[@]}"; do
@@ -106,10 +128,16 @@ for library in "${libraries[@]}"; do
         else
             extra_arguments=""
         fi
-        if [ -d "$library/outs" ]; then
-            echo "Cellranger looks to have completed for $library, skipping. Check if you do not believe this is the case"
-        else
-            echo "cellranger-atac count --id $library --reference $HOME/group/work/ref/hs/GRCh38-hardmasked-optimised-arc/ --fastqs $fastqs --sample $fastq_name --localcores $num_cores $extra_arguments"
+
+        echo ""
+        echo "cellranger-atac count --id $library --reference $HOME/group/work/ref/hs/GRCh38-hardmasked-optimised-arc/ --fastqs $fastqs --sample $fastq_name --localcores $num_cores $extra_arguments"
+        echo "(number of cores will change upon submission)"
+
+        # Ask the user if they want to submit the indices for FASTQ generation
+        echo -e "\033[0;33mINPUT REQUIRED:\033[0m Is this alright? (Y/N)"
+        read -r choice
+        # Process choices
+        if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
             mkdir -p $outs/logs
             # Submit the job to slurm for counting
             sbatch <<EOF
@@ -117,7 +145,7 @@ for library in "${libraries[@]}"; do
 #SBATCH --job-name ${library}
 #SBATCH --output $outs/logs/${library}_counting.out
 #SBATCH --error $outs/logs/${library}_counting.out
-#SBATCH --ntasks=64
+#SBATCH --ntasks=32
 #SBATCH --mem=96000
 #SBATCH --time=24:00:00
 num_cores=\$(nproc)
@@ -125,15 +153,25 @@ cd $outs
 apptainer run -B /fast $container cellranger-atac count --id $library --reference $HOME/group/work/ref/hs/GRCh38-hardmasked-optimised-arc/ --fastqs $fastqs --sample $fastq --localcores \$num_cores $extra_arguments
 rm -r $outs/$library/_* $outs/$library/SC_ATAC_COUNTER_CS
 EOF
+        elif [ "$choice" = "N" ] || [ "$choice" = "n" ]; then
+            :
+        else
+            echo -e "\033[0;31mERROR:\033[0m Invalid choice. Exiting"
+        fi
         # Reset the fastqs variable
         fastqs=""
-        fi
     # Check if the modality GEX appears anywhere in the csv file. cellranger multi will process this
     elif grep -q '.*GEX.*' "${library_folder}/${library}.csv"; then
-        if [ -d "$library/outs" ]; then
-            echo "Cellranger looks to have completed for $library, skipping. Check if you do not believe this is the case"
-        else
-            echo "cellranger multi --id $library --csv ${library_folder}/${library}.csv"
+
+        echo ""
+        echo "cellranger multi --id $library --csv ${library_folder}/${library}.csv --localcores $num_cores"
+        echo "(number of cores will change upon submission)"
+
+        # Ask the user if they want to submit the indices for FASTQ generation
+        echo -e "\033[0;33mINPUT REQUIRED:\033[0m Is this alright? (Y/N)"
+        read -r choice
+        # Process choices
+        if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
             mkdir -p $outs/logs/
             # Submit the job to slurm for counting
             sbatch <<EOF
@@ -141,9 +179,9 @@ EOF
 #SBATCH --job-name ${library}
 #SBATCH --output $outs/logs/${library}_counting.out
 #SBATCH --error $outs/logs/${library}_counting.out
-#SBATCH --ntasks=64
+#SBATCH --ntasks=32
 #SBATCH --mem=96000
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 num_cores=\$(nproc)
 cd $outs
 apptainer run -B /fast $container cellranger multi --id "$library" --csv "${library_folder}/${library}.csv" --localcores \$num_cores
