adbe19a98ec9611dee4c8085c9fe682c5ec627da
Oliver Knight <oliver.knight@charite.de>
2024-02-15 12:23:38 +0100

    Added script to create conda environments, for singularity recipes

---
+++ conda/env_creation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! command -v conda &> /dev/null; then
+    echo "Error: conda is not found in the PATH. Please ensure conda is installed and added to your PATH."
+    exit 1
+fi
+
+
+conda create -y -n oscar_count -c bih-cubi bcl2fastq2 fastqc multiqc bustools
+conda activate oscar_count
+pip install bio
+conda env export > conda/oscar_count.yml
+conda deactivate
+
+####
+
+conda create -y -n oscar_qc -c nvidia python=3.7 cellsnp-lite numpy=1.19 pandas scipy statsmodels openjdk r-base=4.2.3 r-data.table r-matrix bioconductor-genomicranges bioconductor-summarizedexperiment cuda-toolkit cuda-nvcc
+conda activate oscar_qc
+pip install vireoSNP cellbender
+conda env export > conda/oscar_qc.yml
+conda deactivate
