8863636e6855d5d48792754e3a48d59966ab734a
Oliver Knight <oliver.knight@charite.de>
2024-02-16 11:28:13 +0100

    Changed paths

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -19,7 +19,6 @@ if [ -z "$reference" ]; then
     exit 1
 fi
 
-
 num_cores=$(nproc)
 
 cd $PWD
@@ -36,19 +35,19 @@ rm test_run/cellranger-tiny-bcl-1.2.0.tar.gz
 mkdir -p test_run/test_run_scripts/indices/
 wget -O test_run/test_run_scripts/indices/test_run.csv https://cf.10xgenomics.com/supp/cell-exp/cellranger-tiny-bcl-simple-1.2.0.csv --quiet
 
-if [ ! -f "oscar-counting_latest.sif" ]; then
-	echo "Pulling OSCAR counting image, this might take some time..."
+if [ ! -f "oscar-count_latest.sif" ]; then
+	echo "Pulling OSCAR count image, this might take some time..."
 	echo ""
-	apptainer pull --arch amd64 library://romagnanilab/default/oscar-counting:latest
+	apptainer pull library://romagnanilab/default/oscar-count:latest
 fi
 
 cd test_run
 echo "Running cellranger mkfastq"
 echo ""
-apptainer run -B /fast ../oscar-counting_latest.sif cellranger mkfastq --id test_run_fastq --run test_run_bcl/ --csv test_run_scripts/indices/test_run.csv &> mkfastq.log
+apptainer run -B /fast ../oscar-count_latest.sif cellranger mkfastq --id test_run_fastq --run test_run_bcl/ --csv test_run_scripts/indices/test_run.csv &> mkfastq.log
 echo "Running cellranger count"
 echo ""
-apptainer run -B /fast ../oscar-counting_latest.sif cellranger count --id test_run_sample --fastqs test_run_fastq/outs/fastq_path/H35KCBCXY/ --sample test_sample --localcores $num_cores --transcriptome $reference --chemistry SC3Pv3 --no-bam &> count.log
+apptainer run -B /fast ../oscar-count_latest.sif cellranger count --id test_run_sample --fastqs test_run_fastq/outs/fastq_path/H35KCBCXY/ --sample test_sample --localcores $num_cores --transcriptome $reference --chemistry SC3Pv3 --no-bam &> count.log
 
 if [ ! -f "test_run_sample/outs/" ]; then
 	echo "Sucess! Shutting down"
