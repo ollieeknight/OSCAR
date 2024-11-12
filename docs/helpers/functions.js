3069261e7bdd296a44197871eadef57fc14f9604
ollieeknight <oliver.knight@charite.de>
2024-11-12 11:35:53 +0100

    First versions

--- docs/helpers/functions.js
+++ docs/helpers/functions.js
@@ -15,6 +15,21 @@ function indexIncludeTopBar() {
         });
 }
 
+function pagesIncludeTopBar() {
+    fetch('../helpers/topbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('topbar-placeholder').innerHTML = data;
+            document.getElementById('home-link').href = 'index.html';
+            document.getElementById('metadata-link').href = 'pages/metadata_generator.html';
+            document.getElementById('adt-link').href = 'pages/adt_generator.html';
+            document.getElementById('references-link').href = 'pages/references.html';
+        })
+        .catch(error => {
+            console.error('Error fetching topbar:', error);
+        });
+}
+
 
 function fetchLastCommitDate(owner, repo) {
     const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
