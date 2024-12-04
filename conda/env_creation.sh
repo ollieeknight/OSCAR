1fe961999022a38b7e5b44a86fc914d361733349
Oliver Knight <oliver.c.knight@gmail.com>
2024-12-04 19:24:29 +0100

    Updated for pip

--- conda/env_creation.sh
+++ conda/env_creation.sh
@@ -6,9 +6,6 @@ if ! command -v conda &> /dev/null; then
     exit 1
 fi
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
 # Create a new conda environment named 'oscar_count' with specified packages from the 'bih-cubi' channel
 conda create -y -n oscar_count -c bih-cubi \
     bcl2fastq2 \
@@ -29,25 +26,24 @@ conda env export > oscar_count.yml
 conda create -y -n oscar_qc -c nvidia \
     python=3.7 \
     cellsnp-lite \
-    numpy=1.19 \
+    numpy \
     pandas \
     scipy \
     statsmodels \
     openjdk \
-    r-base=4.2.3 \
+    r-base \
     r-data.table \
     r-matrix \
     bioconductor-genomicranges \
     bioconductor-summarizedexperiment \
     cuda-toolkit \
-    cuda-nvcc \
-    compilers
+    cuda-nvcc
 
 # Activate the 'oscar_qc' environment
 conda activate oscar_qc
 
 # Install additional packages using pip
-pip install vireoSNP cellbender mgatk
+pip install vireoSNP cellbender pulp==2.7.0 matplotlib
 
 # Export the 'oscar_qc' environment to a YAML file
 conda env export > oscar_qc.yml
