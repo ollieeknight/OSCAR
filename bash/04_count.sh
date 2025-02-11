7759b7bfca397c7b19e528101061bf5610c788db
Oliver Knight <oliver.c.knight@gmail.com>
2025-02-11 14:05:06 +0100

    ADT debugging

--- bash/04_count.sh
+++ bash/04_count.sh
@@ -172,6 +172,9 @@ EOF
 
                 read fastq_dirs fastq_libraries < <(count_read_adt_csv "${project_libraries}" "${adt_library_csv}")
 
+                echo $fastq_dirs
+                echo $fastq_libraries
+
                 ADT_index_folder=${project_outs}/$library/adt_index
                 echo $ADT_index_folder
                 ADT_outs=${project_outs}/$library/ADT/
@@ -180,6 +183,7 @@ EOF
                 corrected_fastq=$(realpath -m "${fastq_dirs[0]}/../../KITE_corrected")
                 echo $corrected_fastq
                 
+                exit 1
                 if [ "${count_submitted}" = "yes" ]; then
                     sbatch_dependency="--dependency=afterok:${job_id}"
                 else
