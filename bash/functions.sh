fb2260c49b3596e97f699a287115fbcbbe570747
Oliver Knight <oliver.c.knight@gmail.com>
2025-02-20 21:29:15 +0100

    Log messages

--- bash/functions.sh
+++ bash/functions.sh
@@ -508,29 +508,20 @@ extract_n_donors() {
     local project_ids=("$@")
     local n_donors=""
 
-    echo "DEBUG: Starting extract_n_donors function"
-    echo "DEBUG: Library: $library"
-    echo "DEBUG: Project IDs: ${project_ids[*]}"
-
     library=$(echo "$library" | sed -E 's/_(ATAC|GEX)$//')
 
     for project_id in "${project_ids[@]}"; do
         metadata_file="${dir_prefix}/${project_id}/${project_id}_scripts/metadata/metadata.csv"
-        echo "DEBUG: Checking metadata file: $metadata_file"
 
         while IFS=',' read -r assay experiment_id historical_number replicate modality chemistry index_type index species n_donors adt_file || [[ -n "$assay" ]]; do
             expected_library="${assay}_${experiment_id}_exp${historical_number}_lib${replicate}"
-            echo "DEBUG: Expected library: $expected_library"
-            echo "DEBUG: Assay: $assay, Experiment ID: $experiment_id, Historical Number: $historical_number, Replicate: $replicate, Modality: $modality, N Donors: $n_donors"
 
             if [ "$expected_library" == "$library" ]; then
-                echo "DEBUG: Match found. Donor number: $n_donors"
                 break 2
             fi
         done < "$metadata_file"
     done
 
-    echo "DEBUG: Final donor number: $n_donors"
     echo "$n_donors"
 }
 
